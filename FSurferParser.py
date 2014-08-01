84fee8f4bf468f3d9f3c7b204375829ad649372b
wonge05 <edmund.wong@mountsinai.org>
2014-08-01 15:31:34 -0400

    Updated to handle more stats

--- FSurferParser.py
+++ FSurferParser.py
@@ -1,7 +1,7 @@
 '''
-Created on Jul 31, 2014
+Last modified on Aug 1, 2014
 
-@author: edmundwong
+@author: Edmund Wong
 '''
 
 import os
@@ -24,8 +24,10 @@ Commands
 class Usage(Exception):
     def __init__(self, msg=help_message):
         self.msg = msg
+        
 
 def main():
+    
     try:
         opts, args = getopt.getopt(sys.argv[1:], "hi:o:")
     except getopt.error, msg:
@@ -36,38 +38,72 @@ def main():
         if option in ("-i"):
             FSinput = value
         if option in ("-o"):
-            outputCsv = value
+            outputDir= value
     
     os.chdir(FSinput)
+
+    # Automatic Segmentation: Volume (mm^3)
+    createFSCsv("aseg.stats", "aseg.csv", 4, 3, outputDir)
+    
+    # Left Hemiphere Cortical Parcellation: Gray Matter Volume (mm^3)
+    createFSCsv("lh.aparc.stats", "lh_parc_grayVol.csv", 0, 3, outputDir)
+    
+    # Right Hemiphere Cortical Parcellation: Gray Matter Volume (mm^3)
+    createFSCsv("rh.aparc.stats", "rh_parc_grayVol.csv", 0, 3, outputDir)
+    
+    # Left Hemiphere Cortical Parcellation: Surface Area (mm^2)
+    createFSCsv("lh.aparc.stats", "lh_parc_surfArea.csv", 0, 2, outputDir) 
+    
+    # Right Hemiphere Cortical Parcellation: Surface Area (mm^2)
+    createFSCsv("rh.aparc.stats", "rh_parc_surfArea.csv", 0, 2, outputDir)
+    
+    # Left Hemiphere Cortical Parcellation: Average Thickness (mm)
+    createFSCsv("lh.aparc.stats", "lh_parc_avgThickness.csv", 0, 4, outputDir)
+    
+    # Right Hemiphere Cortical Parcellation: Average Thickness (mm)
+    createFSCsv("rh.aparc.stats", "rh_parc_avgThickness.csv", 0, 4, outputDir)
+    
+    # Left Hemiphere Cortical Parcellation: Integrated Rectified Mean Curvature (mm^-1)
+    createFSCsv("lh.aparc.stats", "lh_parc_MeanCurv.csv", 0, 6, outputDir)
+    
+    # Right Hemiphere Cortical Parcellation: Integrated Rectified Mean Curvature (mm^-1)
+    createFSCsv("rh.aparc.stats", "rh_parc_MeanCurv.csv", 0, 6, outputDir)
+    
+
+def createFSCsv(statsfile, outCsvName, label_col, val_col, outputDir):
+    
     wholeList = list()
-    for fsFile in glob.glob("*/stats/aseg.stats"):
+    for fsFile in glob.glob("*/stats/" + statsfile):
         subList = list()
         subList.append("Subject")
-        f=open(fsFile,"r")
+        f = open(fsFile,"r")
         for line in f:
             if (line[0] != "#"):
-                lll = line.split()            
-                subList.append(lll[4])        
+                lll = line.split()
+                subList.append(lll[label_col])
         wholeList.append(subList)
         break
-    for fsFile in glob.glob("*/stats/aseg.stats"):
-        subList=list()
-        f=open(fsFile,"r")
+    
+    for fsFile in glob.glob("*/stats/" + statsfile):
+        subList = list()
+        f = open(fsFile,"r")
         subList.append(fsFile[0:15])
         for line in f:
             if (line[0] != "#"):
                 lll = line.split()            
-                subList.append(lll[3])
+                subList.append(lll[val_col])
         wholeList.append(subList)
         
-
-    with open(outputCsv, 'wb') as fp:
+    with open(outputDir + "/" + outCsvName, 'wb') as fp:
         a = csv.writer(fp, delimiter=',')
         data = wholeList
         a.writerows(data)
-    
-    print "Success. See output file at " + outputCsv
         
-
+    print "Success. See output file at " + outputDir + "/" + outCsvName
+     
+ 
+    
+    
+                  
 if __name__ == "__main__":
     sys.exit(main())
